40472bc5834a5a0a4a41403025b9ce90b6f1783d
Kay Gonschior <kay.gonschior@googlemail.com>
2016-10-13 14:41:15 +0200

    Modify Demo
    Add SelectableInput to demo & display form values

--- build/index.js
+++ build/index.js
@@ -1,16 +1,16 @@
 (function (global, factory) {
   if (typeof define === "function" && define.amd) {
-    define(['exports', './withClassName', './form/BaseField', './form/FormValueScope', './form/Form', './form/CheckboxField', './form/DropDownField', './form/ErrorMessage', './form/InputField', './form/IteratorField', './form/RadioButtonField', './form/RangeField', './form/SortField', './form/TextareaField', './form/ChoiceField'], factory);
+    define(['exports', './withClassName', './form/BaseField', './form/FormValueScope', './form/Form', './form/CheckboxField', './form/DropDownField', './form/ErrorMessage', './form/InputField', './form/IteratorField', './form/RadioButtonField', './form/RangeField', './form/SortField', './form/TextareaField', './form/ChoiceField', './form/SelectableInput'], factory);
   } else if (typeof exports !== "undefined") {
-    factory(exports, require('./withClassName'), require('./form/BaseField'), require('./form/FormValueScope'), require('./form/Form'), require('./form/CheckboxField'), require('./form/DropDownField'), require('./form/ErrorMessage'), require('./form/InputField'), require('./form/IteratorField'), require('./form/RadioButtonField'), require('./form/RangeField'), require('./form/SortField'), require('./form/TextareaField'), require('./form/ChoiceField'));
+    factory(exports, require('./withClassName'), require('./form/BaseField'), require('./form/FormValueScope'), require('./form/Form'), require('./form/CheckboxField'), require('./form/DropDownField'), require('./form/ErrorMessage'), require('./form/InputField'), require('./form/IteratorField'), require('./form/RadioButtonField'), require('./form/RangeField'), require('./form/SortField'), require('./form/TextareaField'), require('./form/ChoiceField'), require('./form/SelectableInput'));
   } else {
     var mod = {
       exports: {}
     };
-    factory(mod.exports, global.withClassName, global.BaseField, global.FormValueScope, global.Form, global.CheckboxField, global.DropDownField, global.ErrorMessage, global.InputField, global.IteratorField, global.RadioButtonField, global.RangeField, global.SortField, global.TextareaField, global.ChoiceField);
+    factory(mod.exports, global.withClassName, global.BaseField, global.FormValueScope, global.Form, global.CheckboxField, global.DropDownField, global.ErrorMessage, global.InputField, global.IteratorField, global.RadioButtonField, global.RangeField, global.SortField, global.TextareaField, global.ChoiceField, global.SelectableInput);
     global.index = mod.exports;
   }
-})(this, function (exports, _withClassName, _BaseField, _FormValueScope, _Form, _CheckboxField, _DropDownField, _ErrorMessage, _InputField, _IteratorField, _RadioButtonField, _RangeField, _SortField, _TextareaField, _ChoiceField) {
+})(this, function (exports, _withClassName, _BaseField, _FormValueScope, _Form, _CheckboxField, _DropDownField, _ErrorMessage, _InputField, _IteratorField, _RadioButtonField, _RangeField, _SortField, _TextareaField, _ChoiceField, _SelectableInput) {
   'use strict';
 
   Object.defineProperty(exports, "__esModule", {
@@ -98,6 +98,12 @@
     enumerable: true,
     get: function () {
       return _interopRequireDefault(_ChoiceField).default;
+    }
+  });
+  Object.defineProperty(exports, 'SelectableInput', {
+    enumerable: true,
+    get: function () {
+      return _interopRequireDefault(_SelectableInput).default;
     }
   });
 
